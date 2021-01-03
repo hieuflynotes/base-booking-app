026a80d6d4be8f0b10b4e7f0ef9ca8593f1eecee
hieuflynotes <hieu@flynotes.co.uk>
2021-01-03 02:32:43 +0000

    add model for role & permission

---
+++ user/model/Permission.ts
@@ -0,0 +1,6 @@
+import { BaseModel } from "@BaseTypes/model/BaseModel";
+
+export class Permission extends BaseModel {
+  name: string;
+  desc: string;
+}
---
+++ user/model/Role.ts
@@ -0,0 +1,5 @@
+import { BaseModel } from "@BaseTypes/model/BaseModel";
+
+export class Role extends BaseModel{
+    
+}
