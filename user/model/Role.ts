1459a5d61a503b45e61204fb2f7ec17aa3408a24
hieuflynotes <hieu@flynotes.co.uk>
2021-01-03 03:06:42 +0000

    add example filter role

--- user/model/Role.ts
+++ user/model/Role.ts
@@ -1,5 +1,13 @@
 import { BaseModel } from "@BaseTypes/model/BaseModel";
+import { ListFilter } from "@BaseTypes/model/Filter";
 
-export class Role extends BaseModel{
-    
+export class Role extends BaseModel {
+  name: string;
+  desc: string;
+}
+
+const filterRole : ListFilter<Role> = {
+    fieldFilters:{
+        name: "ho hieu"
+    }
 }
