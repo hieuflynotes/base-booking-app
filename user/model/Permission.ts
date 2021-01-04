20ecdaf78e7f4f0f0e698cd30f1ef8842731a484
lgnvpr <=>
2021-01-04 05:22:49 +0000

    parse deep

--- user/model/Permission.ts
+++ user/model/Permission.ts
@@ -1,6 +1,9 @@
 import { BaseModel } from "@BaseTypes/model/BaseModel";
+import { Role } from "./Role";
 
 export class Permission extends BaseModel {
   name: string;
   desc: string;
+  roleId : string;
+  role ?: Role
 }
