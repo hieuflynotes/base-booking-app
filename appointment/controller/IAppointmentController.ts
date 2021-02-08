80b703d3df4414f114cb02ad6b3b7f179cabdd07
kylm1403 <ktdraft0902@gmail.com>
2021-02-08 08:45:49 +0700

    change model

--- appointment/controller/IAppointmentController.ts
+++ appointment/controller/IAppointmentController.ts
@@ -1,4 +1,15 @@
 import { IBaseController } from "@BaseTypes/controller/IBaseController";
+import { ListFilter } from "@BaseTypes/model/Filter";
+import { Paging } from "@BaseTypes/model/Paging";
 import { Appointment } from "../model/Appointment";
 
-export interface IAppointmentController extends IBaseController<Appointment> {}
+export interface IAppointmentController extends IBaseController<Appointment> {
+	getGroupAppointmentByDate(
+		params: ListFilter<Appointment> & { from: Date; to: Date }
+	): Promise<Paging<AppointmentGroupByDate>>;
+}
+
+type AppointmentGroupByDate = {
+	date: Date;
+	data: Appointment[];
+};
